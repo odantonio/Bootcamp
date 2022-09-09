6cbef944c8e4ebe964579b827a3c037120c3560c
odantonio <obdantonio@terra.com.br>
2022-09-09 07:59:03 -0300

    sincronizando rep

---
+++ Javascript/Contador/Resources/js/main.js
@@ -0,0 +1,15 @@
+//var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+document.getElementById("sobe").addEventListener("click", increment { document.getElementById("currentNumber").innerHTML = currentNumber + 1 });
+document.getElementById("desce").addEventListener("click", decrement { document.getElementById("currentNumber").innerHTML = currentNumber - 1 });
+
+
+/* function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+} */
+
+/* function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+} */
